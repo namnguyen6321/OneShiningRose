b84cc9b9948fb2cce624d28861f27e96dcec027b
namnguyen6321 <nguyendinhnama1k51dta@gmail.com>
2025-09-29 14:58:26 +0700

    update

--- backend/src/video/video.module.ts
+++ backend/src/video/video.module.ts
@@ -1,14 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { VideoController } from './video.controller';
-// import { VideoService } from './video.service';
-// import { PrismaService } from '../prisma.service';
-
-// @Module({
-//   controllers: [VideoController],
-//   providers: [VideoService, PrismaService],
-// })
-// export class VideoModule {}
-
 import { Module } from '@nestjs/common';
 import { VideoController } from './video.controller';
 import { VideoService, VIDEO_REPOSITORY } from './video.service';
@@ -23,13 +12,6 @@ import { PrismaVideoRepository } from './adapters/prisma-video.repository';
     {
       provide: VIDEO_REPOSITORY,
       useClass: PrismaVideoRepository,
-      // useFactory: (prisma: PrismaService) => {
-      //   const useMock =
-      //     !process.env.DATABASE_URL || process.env.USE_MOCK_DB === '1';
-      //   if (useMock) return new MemoryVideoRepository();
-      //   return new PrismaVideoRepository(prisma);
-      // },
-      // inject: [PrismaService],
     },
   ],
   exports: [VideoService],
